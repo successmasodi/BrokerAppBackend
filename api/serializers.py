f50e52e11a90f92ce02be050ac6c3caa3d5f38f5
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-02-27 11:35:55 +0100

    add validation to Deposit and Withdrawal serializers for balance checks

--- api/serializers.py
+++ api/serializers.py
@@ -1,17 +1,27 @@
 from rest_framework import serializers
 from transactions.models import Balance, Deposit, Withdrawal
+from decimal import Decimal
 
 class BalanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Balance
         fields = ['id', 'user', 'amount']
-        read_only_fields = ['id', 'user', 'amount']  # Make all fields read-only
+        read_only_fields = ['id', 'user', 'amount']
 
 class DepositSerializer(serializers.ModelSerializer):
     class Meta:
         model = Deposit
         fields = ['id', 'user', 'amount', 'timestamp']
         read_only_fields = ['id', 'timestamp', 'user']
+
+    def validate(self, data):
+        user = self.context['request'].user
+        balance, created = Balance.objects.get_or_create(user=user)
+        if Decimal(balance.amount) + Decimal(data['amount']) < 0:
+            raise serializers.ValidationError({'error': 'Insufficient balance after deposit.'})
+        return data
+
+
 
 class WithdrawalSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,4 +30,12 @@ class WithdrawalSerializer(serializers.ModelSerializer):
         read_only_fields = ['id', 'timestamp', 'user']
 
     def validate(self, data):
+        user = self.context['request'].user
+        try:
+            balance = Balance.objects.get(user=user)
+            if Decimal(data['amount']) > Decimal(balance.amount):
+                raise serializers.ValidationError({'error': 'Insufficient balance for this withdrawal.'})
+        except Balance.DoesNotExist:
+            raise serializers.ValidationError({'error': 'Insufficient balance for this withdrawal.'})
         return data
+
