0443371771e577659be60150699d7ee143f0f277
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2025-03-12 15:31:50 +0100

    perm: updated the permission on deposit viewset to allow only owner to be able to modify their object

--- api/permissions.py
+++ api/permissions.py
@@ -1,5 +1,15 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
-class IsAuthenticated(BasePermission):
+
+class IsAuthenticated(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user and request.user.is_authenticated
+
+
+class IsOwnerOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
+    """A user is allowed to modify their own object e.g deposit..."""
+
+    def has_object_permission(self, request, view, obj):
+        if request.method and request.method not in permissions.SAFE_METHODS:
+            return bool(obj.user == request.user)
+        return super().has_object_permission(request, view, obj)
