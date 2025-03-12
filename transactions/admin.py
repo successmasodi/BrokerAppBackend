d284fa5c9f6b0791387b8ac30532b95273621c59
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2025-03-12 15:30:42 +0100

    admin: added the is_verified to display on deposit table

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import Deposit, Withdrawal, Balance
 
-admin.site.register(Deposit)
+# admin.site.register(Deposit)
 admin.site.register(Withdrawal)
 admin.site.register(Balance)
 
 # Register your models here.
+
+
+@admin.register(Deposit)
+class DepositAdmin(admin.ModelAdmin):
+    list_display = ("user", "amount", "is_verified")
+    list_editable = ("is_verified",)
+    list_per_page = 10
+    list_filter = ("is_verified",)
+    search_fields = ("user__username",)
+
+    class Meta:
+        model = Deposit
